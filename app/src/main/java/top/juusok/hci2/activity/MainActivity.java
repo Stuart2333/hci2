7b281356694eb425dc624f7d746dcbd26d168df6
JuusoK <JuusoK@bitbucket.org>
2018-12-04 18:09:33 +0800

    init submit

---
+++ app/src/main/java/top/juusok/hci2/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package top.juusok.hci2.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import top.juusok.hci2.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
